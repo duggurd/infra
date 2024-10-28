05ec23335598d97879ea47122ac0e556db4c1687
duggurd <alexanderhaugerud@gmail.com>
2024-10-28 22:57:16 +0100

    cleanup finn_metadata ingestion

--- kubernetes/airflow/dags/finn_metadata/dag.py
+++ kubernetes/airflow/dags/finn_metadata/dag.py
@@ -4,8 +4,6 @@ from airflow.datasets import Dataset
 from datetime import datetime
 import json
 
-from utils.minio_utils import connect_to_minio
-
 with open("/opt/airflow/dags/finn_metadata/finn_ingestion_config.json") as f:
     search_keys = json.load(f)["search_keys"]
 
@@ -21,9 +19,24 @@ datasets = [Dataset(search_key) for search_key in search_keys]
 def ingest_finn():
 
     for search_key in search_keys:
-        @task(task_id=f"ingest_{search_key}")
+        @task.virtualenv(
+            task_id=f"ingest_{search_key}",
+            requirements=[
+                "pandas",
+                "minio",
+                "pyarrow",
+                "s3fs"
+            ],
+            system_site_packages=False,
+            venv_cache_path="/tmp/venv/cache/finn_metadata",
+            pip_install_options=["--require-virtualenv", "--isolated"]
+        )
         def ingest(search_key):
+            import sys
+            sys.path.append('/opt/airflow/dags/finn_metadata')
+            sys.path.append('/opt/airflow/dags')
             from finn_metadata.ingest import ingest_new_finn_ads
+            from utils.minio_utils import connect_to_minio
             client = connect_to_minio()
             ingest_new_finn_ads(client, search_key)
     
