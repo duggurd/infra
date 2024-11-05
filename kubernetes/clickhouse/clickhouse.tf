bf472f28e52c73e26674d6b4a60214765f6ccca1
duggurd <alexanderhaugerud@gmail.com>
2024-11-05 16:17:01 +0100

    clickhouse deployment

--- kubernetes/clickhouse/clickhouse.tf
+++ kubernetes/clickhouse/clickhouse.tf
@@ -1,75 +1,135 @@
-resource "kubernetes_namespace" "clickhouse_ns" {
+resource "kubernetes_namespace" "clickhouse" {
   metadata {
-    name = "clickhouse-ns"
+    name = "clickhouse"
   }
 }
 
-resource "kubernetes_persistent_volume_claim" "clickhouse_pvc" {
-   metadata {
-       name = "clickhouse-pvc"
-       namespace = kubernetes_namespace.clickhouse_ns.metadata.0.name 
-   }
-   spec {
-       access_modes = ["ReadWriteOnce"]
-        resources {
-          requests = {
-            storage = "50Gi"
+resource "kubernetes_persistent_volume" "clickhouse" {
+  metadata {
+    name = "clickhouse"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    capacity = {
+      storage = "50Gi"
+    }
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = "local-path"
+    persistent_volume_source {
+      local {
+        path = "/mnt/clickhouse"
+      }
+    }
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = ["homelab.alpine2"]
           }
         }
-      # volume_name = "${kubernetes_persistent_volume.clickhouse_pv.metadata.0.name}"
-   }
-   wait_until_bound = false
+      }
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "clickhouse" {
+  metadata {
+    name      = "clickhouse"
+    namespace = kubernetes_namespace.clickhouse.metadata.0.name
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "50Gi"
+      }
+    }
+    volume_name        = kubernetes_persistent_volume.clickhouse.metadata.0.name
+    storage_class_name = "local-path"
+  }
+  wait_until_bound = false
 }
 
 
-resource "kubernetes_pod" "clickhouse_pod" {
+resource "kubernetes_deployment" "clickhouse" {
+  timeouts {
+    create = "2m"
+  }
   metadata {
-    name = "clickhouse-pod"
-    namespace = kubernetes_namespace.clickhouse_ns.metadata.0.name
+    name      = "clickhouse"
+    namespace = kubernetes_namespace.clickhouse.metadata[0].name
     labels = {
-      "app" = "clickhouse" 
+      app = "clickhouse"
     }
   }
   spec {
-    container {
-      name = "clickhouse"    
-      image = "clickhouse/clickhouse-server:head-alpine"
-      image_pull_policy = "IfNotPresent"
-
-      port {
-       container_port = 8123 
-      }
-
-      volume_mount {
-       mount_path = "/var/lib/clickhouse/" 
-       name = "clickhouse-data"
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "clickhouse"
       }
     }
-
-    volume {
-      name = "clickhouse-data"
-      persistent_volume_claim {
-       claim_name = kubernetes_persistent_volume_claim.clickhouse_pvc.metadata.0.name 
-      } 
+    template {
+      metadata {
+        labels = {
+          app = "clickhouse"
+        }
+      }
+      spec {
+        node_selector = {
+          "kubernetes.io/hostname" = "homelab.alpine2"
+        }
+        container {
+          name              = "clickhouse"
+          image             = "clickhouse/clickhouse-server:head-alpine"
+          image_pull_policy = "IfNotPresent"
+          port {
+            host_port      = 8123
+            container_port = 8123
+          }
+          volume_mount {
+            name       = "clickhouse-data"
+            mount_path = "/var/lib/clickhouse/"
+          }
+          resources {
+            requests = {
+              cpu    = "1"
+              memory = "512m"
+            }
+            limits = {
+              cpu    = "2"
+              memory = "2Gi"
+            }
+          }
+        }
+        volume {
+          name = "clickhouse-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.clickhouse.metadata[0].name
+          }
+        }
+      }
     }
-  } 
+  }
 }
 
-
-resource "kubernetes_service" "clickhouse_svc" {
- metadata {
-  name = "clickhouse-svc" 
-  namespace = kubernetes_namespace.clickhouse_ns.metadata.0.name
- }   
- spec {
-   selector = {
-     "app" = "clickhouse"
-   }
-  type = "NodePort"  
-  port {
-    port = 8123
-    target_port = 8123
+resource "kubernetes_service" "clickhouse" {
+  metadata {
+    name      = "clickhouse-svc"
+    namespace = kubernetes_namespace.clickhouse.metadata.0.name
   }
- }
+  spec {
+    selector = {
+      "app" = "clickhouse"
+    }
+    type = "NodePort"
+    port {
+      port        = 8123
+      target_port = 8123
+      node_port   = 30123
+    }
+  }
 }
 
